9c2943c53757a620137e29ae5235de5c134e1b6b
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-23 13:38:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,25 +1,55 @@
 class Solution 
 {
 public:
+
+    double solve(vector<int>&left, int n, vector<int>&right, int m)
+    {
+        int low = 0;
+        int high = n;
+        int total = (n+m+1)/2;
+        while(low<=high)
+        {
+            int first = low+(high-low)/2;
+            int second = total - first;
+
+            int a = first==0 ? INT_MIN : left[first-1];
+            int b = first==n ? INT_MAX : left[first];
+
+            int c = second==0 ? INT_MIN : right[second-1];
+            int d = second==m ? INT_MAX : right[second];
+
+            if(a <=d && c<=b)
+            {
+                if((m+n)%2 == 0)
+                {
+                    return (max(a,c) + min(b, d))/2.0;
+                }
+                else
+                {
+                    return max(a,c)/1.0;
+                }
+            }
+            if(a > d)
+            {
+                high = first - 1;
+            }
+            else
+            {
+                low = first + 1;
+            }
+        }
+        return 0.0;
+    }
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
     {
-        vector<int>temp;
-        for(int num : nums1)
+        int l1 = nums1.size();
+        int l2 = nums2.size();
+
+        if(l1 < l2)
         {
-            temp.push_back(num);
+            return solve(nums1, l1, nums2, l2);
         }
-        for(int num : nums2)
-        {
-            temp.push_back(num);
-        }
-        sort(temp.begin(), temp.end());
-        int n = temp.size();
-        if(n%2!=0)
-        {
-            return (double)temp[n/2];
-        }
-        double a = temp[n/2];
-        double b = temp[(n/2)-1];
-        return (a+b)/2;
+        return solve(nums2, l2, nums1, l1);
     }
 };
