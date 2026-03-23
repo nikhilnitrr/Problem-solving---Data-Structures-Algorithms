c704287e4e9a02abbdc0397970489cd53cde3372
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-23 18:47:59 +0530

    Time: 2 ms (52.4%), Space: 25.2 MB (75.85%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+public:
+    vector<int> findErrorNums(vector<int>& arr) 
+    {
+        int n = arr.size();
+        int i = 0;
+        while(i<n)
+        {
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct])
+            {
+                swap(arr[i], arr[correct]);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=i+1)
+            {
+                return {arr[i], i+1};
+            }
+        }
+        return {};
+    }
+};
