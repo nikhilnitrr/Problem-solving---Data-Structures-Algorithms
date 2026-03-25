e32854fed392e1d82e4efed4b10d0e2af762c5c9
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-25 08:19:23 +0530

    Time: 471 ms (5.05%), Space: 131.1 MB (13.85%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -0,0 +1,44 @@
+class DetectSquares 
+{
+public:
+
+    bool is_diagonal(vector<int>&first, vector<int>&second)
+    {
+        if(abs(first[0] - second[0]) == abs(first[1] - second[1]) && first!=second)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    map<vector<int>, int>um;
+
+    DetectSquares() 
+    {
+        um.clear();
+    }
+    
+    void add(vector<int> point) 
+    {
+        um[point]++;
+    }
+    
+    int count(vector<int> point) 
+    {
+        int result = 0;
+        for(auto p : um)
+        {
+            vector<int>temp = p.first;
+            int cnt = p.second;
+
+            if(is_diagonal(point, temp))
+            {
+                if(um.find({point[0], temp[1]})!=um.end() && um.find({temp[0], point[1]})!=um.end())
+                {
+                    result+=(cnt*um[{point[0], temp[1]}]*um[{temp[0], point[1]}]);
+                }
+            }
+        }
+        return result;
+    }
+};
