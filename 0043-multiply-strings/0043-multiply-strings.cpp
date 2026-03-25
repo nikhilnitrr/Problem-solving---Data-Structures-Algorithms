0ff37204e9f35d981babe42ca6dc0978c898c569
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-25 10:19:37 +0530

    Time: 3 ms (60.41%), Space: 10.9 MB (17.87%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,43 +3,30 @@ class Solution
 public:
     string multiply(string num1, string num2) 
     {
-        if(num1=="0" || num2=="0")
-        {
-            return "0";
-        }
+        if(num1=="0" || num2=="0") return "0";
         int n = num1.length();
         int m = num2.length();
-        
-        vector<int>result(n+m, 0);
 
-        int i = n-1;
-        int j = m-1;
+        string result = "";
+        vector<int>temp(n+m, 0);
 
-        while(j>=0)
+        for(int j=n-1;j>=0;j--)
         {
-            i=n-1;
-            int a = (int)(num2[j]-'0');
-            while(i>=0)
-            {
-                int b = (int)(num1[i]-'0');
-
-                int mul = a*b;
-
-                int sum = mul + result[i+j+1];
-
-                result[i+j+1] = sum%10;
-                result[i+j]+=sum/10;
-                
-                i--;
+            int a = (int)(num1[j]-'0');
+            for(int i=m-1;i>=0;i--)
+            { 
+                int b = (int)(num2[i]-'0');
+                int mul = (a*b);
+                int sum = mul + temp[i+j+1];
+                temp[i+j+1] = sum%10;
+                temp[i+j] += sum/10;
             }
-            j--;
+        }
 
-        }
-        string ans = "";
-        for(int i=0;i<result.size();i++)
+        for(int i=0;i<temp.size();i++)
         {
-            ans+=to_string(result[i]);
+            result+=to_string(temp[i]);
         }
-        return ans[0]=='0' ? ans.substr(1) : ans;
+        return result[0]=='0' ? result.substr(1) : result;
     }
 };
