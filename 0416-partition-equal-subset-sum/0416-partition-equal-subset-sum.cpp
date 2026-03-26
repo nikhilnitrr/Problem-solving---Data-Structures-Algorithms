378015f1bdecfdc7d09824a1833788578d6c69d8
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-26 09:36:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,41 +4,39 @@ public:
 
     bool solve(vector<int>&nums, int n, int target, vector<vector<int>>&dp)
     {
-        if(target < 0)
+        if(n <=0)
         {
             return false;
         }
-        if(target == 0)
+        if(target <= 0)
         {
             return true;
         }
-        if(n<=0)
-        {
-            return false;
-        }
-
         if(dp[n][target]!=-1)
         {
             return dp[n][target];
         }
-
         bool not_take = solve(nums, n-1, target, dp);
-
         bool take = false;
-
         if(nums[n-1] <= target)
         {
             take = solve(nums, n-1, target-nums[n-1], dp);
         }
-
-        return dp[n][target] = take || not_take;
+        return dp[n][target] = (take || not_take);
     }
 
     bool canPartition(vector<int>& nums) 
     {
+        int sum = 0;
         int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum%2 !=0) return false;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum += nums[i];
+        }
+        if(sum%2 != 0)
+        {
+            return false;
+        }
         int target = sum/2;
         vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
         return solve(nums, n, target, dp);
