aeefb18f0c9bdb2aa8415ad7c8872b78fa06bdc3
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-26 13:34:31 +0530

    Time: 846 ms (64.61%), Space: 353.1 MB (70.77%) - LeetHub

---
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.cpp
@@ -0,0 +1,99 @@
+#define ll long long int
+class Solution 
+{
+public:
+
+    bool is_horizontal_cut(ll total, vector<vector<int>>& grid, unordered_set<ll>&st)
+    {
+        ll n = grid.size();
+        ll m = grid[0].size();
+        ll top = 0;
+
+        for(ll i=0;i<n-1;i++)
+        {
+            for(ll j=0;j<m;j++)
+            {
+                st.insert(grid[i][j]);
+                top+=grid[i][j];
+            }
+            ll bottom = total - top;
+            ll diff = top - bottom;
+            if(diff == 0)
+            {
+                return true;
+            }
+            if(diff == grid[0][0])
+            {
+                return true;
+            }
+            if(diff == grid[0][m-1])
+            {
+                return true;
+            }
+            if(diff == grid[i][0])
+            {
+                return true;
+            }
+            if(st.find(diff)!=st.end() && m > 1 && i > 0)
+            {
+                return true;
+            }
+        }
+        st.clear();
+        return false;
+    }
+
+    void transpose(vector<vector<int>>&grid)
+    {
+        ll n = grid.size();
+        ll m = grid[0].size();
+
+        vector<vector<int>>temp(m, vector<int>(n));
+
+        for(ll i=0;i<n;i++)
+        {
+            for(ll j=0;j<m;j++)
+            {
+                temp[j][i] = grid[i][j];
+            }
+        }
+
+        grid = temp;
+    }
+
+    bool canPartitionGrid(vector<vector<int>>& grid) 
+    {
+        ll n = grid.size();
+        ll m = grid[0].size();
+        unordered_set<ll>st;
+
+        ll total = 0;
+        for(ll i=0;i<n;i++)
+        {
+            for(ll j=0;j<m;j++)
+            {
+                total += grid[i][j];
+            }
+        }
+        if(is_horizontal_cut(total, grid, st))
+        {
+            return true;
+        }
+        reverse(grid.begin(), grid.end());
+        if(is_horizontal_cut(total, grid, st))
+        {
+            return true;
+        }
+        transpose(grid);
+        if(is_horizontal_cut(total, grid, st))
+        {
+            return true;
+        }
+        reverse(grid.begin(), grid.end());
+        if(is_horizontal_cut(total, grid, st))
+        {
+            return true;
+        }
+        return false;
+    }
+};
