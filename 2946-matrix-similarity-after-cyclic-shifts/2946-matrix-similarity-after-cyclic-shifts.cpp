5255381bb859ae23517c8d9af8afa77131d9e6fa
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-27 08:16:48 +0530

    Time: 4 ms (22.98%), Space: 32.6 MB (26.09%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,45 @@
+class Solution 
+{
+public:
+
+    void shift_left(vector<int>&arr, int k)
+    {
+        int n = arr.size();
+        k= k%n;
+        reverse(arr.begin(), arr.begin()+k);
+        reverse(arr.begin()+k, arr.end());
+        reverse(arr.begin(), arr.end());
+    }
+    void shift_right(vector<int>&arr, int k)
+    {
+        int n = arr.size();
+        k = k%n;
+        reverse(arr.begin(), arr.end());
+        reverse(arr.begin(), arr.begin()+k);
+        reverse(arr.begin()+k, arr.end());
+    }
+
+    bool areSimilar(vector<vector<int>>& arr, int k) 
+    {
+        int n = arr.size();
+        int m = arr[0].size();
+        vector<vector<int>>temp = arr;
+
+        for(int i=0;i<n;i++)
+        {
+            if(i%2 == 0)
+            {
+                shift_left(temp[i], k);
+            }
+            else
+            {
+                shift_right(temp[i], k);
+            }
+        }
+        if(temp == arr)
+        {
+            return true;
+        }
+        return false;
+    }
+};
