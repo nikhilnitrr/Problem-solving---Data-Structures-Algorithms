66363477c059133474575658c081a3d6587c6651
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-27 08:20:19 +0530

    Time: 0 ms (100%), Space: 32.6 MB (33.54%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -5,6 +5,7 @@ public:
     void shift_left(vector<int>&arr, int k)
     {
         int n = arr.size();
+        k=k%n;
         reverse(arr.begin(), arr.begin()+k);
         reverse(arr.begin()+k, arr.end());
         reverse(arr.begin(), arr.end());
@@ -12,6 +13,7 @@ public:
     void shift_right(vector<int>&arr, int k)
     {
         int n = arr.size();
+        k=k%n;
         reverse(arr.begin(), arr.end());
         reverse(arr.begin(), arr.begin()+k);
         reverse(arr.begin()+k, arr.end());
@@ -21,16 +23,15 @@ public:
     {
         int n = arr.size();
         int m = arr[0].size();
-        k = k%n;
         vector<vector<int>>temp = arr;
 
         for(int i=0;i<n;i++)
         {
-            if(i%2 == 0 && k!=n)
+            if(i%2 == 0)
             {
                 shift_left(temp[i], k);
             }
-            else if(i%2 !=0 && k!=n)
+            else if(i%2 !=0)
             {
                 shift_right(temp[i], k);
             }
