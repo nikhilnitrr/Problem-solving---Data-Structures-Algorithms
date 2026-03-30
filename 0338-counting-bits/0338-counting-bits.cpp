21b1164bce7859fa95e3d52b115a9ec5acd08c68
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-30 14:01:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,26 +1,12 @@
 class Solution 
 {
 public:
-
-    int get_count(int num)
-    {
-        int cnt = 0;
-        while(num > 1)
-        {
-            cnt+=(num&1);
-            num=num>>1;
-        }
-        if(num==1)cnt++;
-        return cnt;
-    }
-
     vector<int> countBits(int n) 
     {
-        vector<int>ans;
-        for(int i=0;i<=n;i++)
+        vector<int>ans(n+1, 0);
+        for(int i=1;i<=n;i++)
         {
-            int res = get_count(i);
-            ans.push_back(res);
+            ans[i] = (ans[i>>1] + (i&1));
         }
         return ans;
     }
