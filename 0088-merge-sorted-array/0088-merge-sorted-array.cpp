8959d154b46086e87fff9a9be31504e644151f25
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-30 18:40:54 +0530

    Time: 0 ms (100%), Space: 12.3 MB (41.08%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        int pointer1 = m-1;
+        int pointer2 = n-1;
+        int pointer = m+n-1;
+
+        while(pointer2 >=0)
+        {
+            if(pointer1 >=0 && nums1[pointer1] >= nums2[pointer2])
+            {
+                nums1[pointer] = nums1[pointer1];
+                pointer1--;
+            }
+            else
+            {
+                nums1[pointer] = nums2[pointer2];
+                pointer2--;
+            }
+            pointer--;
+        }
+    }
+};
