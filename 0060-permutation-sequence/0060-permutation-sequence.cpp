66a64c311d8001e5392c944d2cbfcbe905207e78
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-31 11:28:48 +0530

    Time: 80 ms (9.95%), Space: 8.5 MB (74.18%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,56 @@
+class Solution 
+{
+public:
+
+    void solve(string &str, int k, int n)
+    {
+        int cnt = 1;
+        while(cnt < k)
+        {
+            int indx1 = -1;
+            for(int i=n-2;i>=0;i--)
+            {
+                if(str[i] < str[i+1])
+                {
+                    indx1 = i;
+                    break;
+                }
+            }
+            if(indx1 == -1)
+            {
+                reverse(str.begin(), str.end());
+                cnt++;
+            }
+            else
+            {
+                int indx2 = -1;
+                for(int i=n-1;i>=0;i--)
+                {
+                    if(str[i] > str[indx1])
+                    {
+                        indx2 = i;
+                        break;
+                    }
+                }
+                swap(str[indx1], str[indx2]);
+                reverse(str.begin()+indx1+1, str.end());
+                cnt++;
+            }
+        }
+    }
+
+    string getPermutation(int n, int k) 
+    {
+        string str="";
+        for(int i=1;i<=n;i++)
+        {
+            str+=to_string(i);
+        }
+        if(k==0)
+        {
+            return str;
+        }
+        solve(str, k, n);
+        return str;
+    }
+};
