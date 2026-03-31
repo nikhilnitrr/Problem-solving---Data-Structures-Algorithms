7bf5699118a7c66901189b71ec4282b3cffd8ac2
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-03-31 08:02:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,15 +3,16 @@ class Solution
 public:
     void solve(int pos, vector<int>&nums, int n, vector<int>&path, vector<vector<int>>&result)
     {
-        if(find(result.begin(), result.end(), path) == result.end())
-        {
-            result.push_back(path);
-        }
+        result.push_back(path);
         for(int i=pos;i<n;i++)
         {
-            path.push_back(nums[i]);
-            solve(i+1, nums, n, path, result);
-            path.pop_back();
+            if(i > pos && nums[i] != nums[i-1]) continue;
+            else
+            {
+                path.push_back(nums[i]);
+                solve(i+1, nums, n, path, result);
+                path.pop_back();
+            }
         }
     }
 
