50f2787b9dabeb77787ccfb723207c07d9762466
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-04-05 19:05:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,57 @@
+class Solution 
+{
+public:
+
+    bool is_valid(int k, vector<int>&weights, int days)
+    {
+        int cnt = 1;
+        int sum = 0;
+        for(int i=0;i<weights.size();i++)
+        {
+            if(sum+weights[i] <=k)
+            {
+                sum = sum + weights[i];
+            }
+            else
+            {
+                cnt++;
+                sum=weights[i];
+                if(cnt > days)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) 
+    {
+        int n = weights.size();
+        int ans = INT_MAX;
+        int low=weights[0];
+        for(int i=1;i<n;i++)
+        {
+            low=max(low, weights[i]);
+        }
+        int high=0;
+        for(int i=0;i<n;i++)
+        {
+            high+=weights[i];
+        }
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if(is_valid(mid, weights, days))
+            {
+                ans = mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
