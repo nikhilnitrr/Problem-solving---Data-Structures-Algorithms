650ff15d145767be716cfa58a08f973717b34351
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-04-09 09:32:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -6,19 +6,17 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int>prev = matrix[0];
         for(int i=1;i<n;i++)
         {
-            vector<int>curr = matrix[i];
             for(int j=1;j<m;j++)
             {
-                if(curr[j]!=prev[j-1])
+                if(matrix[i][j]!=matrix[i-1][j-1])
                 {
                     return false;
                 }
             }
-            prev=curr;
         }
+
         return true;
     }
 };
