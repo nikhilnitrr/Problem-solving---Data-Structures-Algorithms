58c06b351bca9d78b7c4f832d5b4a5e207fc9e2c
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-04-09 08:58:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,29 +1,63 @@
+struct TrieNode
+{
+    vector<string>suggestions; // top 3 suggestions
+    TrieNode* child[26];
+
+    TrieNode(){
+        for(int i=0;i<26;i++)
+        {
+            child[i] = NULL;
+        }
+    }
+};
+
+void insert(string word, TrieNode* root)
+{
+    TrieNode* curr = root;
+    for(int i=0;i<word.length();i++)
+    {
+        int index = word[i]-'a';
+        if(curr->child[index]==NULL)
+        {
+            curr->child[index] = new TrieNode();
+        }
+        curr = curr->child[index];
+        if(curr->suggestions.size() < 3)
+        {
+            curr->suggestions.push_back(word);
+        }
+    }
+}
+
+
 class Solution 
 {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) 
     {
-        int n = products.size();
-        vector<vector<string>>result;
+        TrieNode* root = new TrieNode();
         sort(products.begin(), products.end());
-
-        for(int len=1;len<=searchWord.length();len++)
+        for(string product : products)
         {
-            string prefix = searchWord.substr(0, len);
-            vector<string>temp;
-            for(int i=0;i<products.size();i++)
-            {
-                if(products[i].substr(0, len)==prefix)
-                {
-                    temp.push_back(products[i]);
-                    if(temp.size()==3)
-                    {
-                        break;
-                    }
-                }
-            }
-            result.push_back(temp);
+            insert(product, root);
         }
+
+        vector<vector<string>>result;
+        TrieNode* curr = root;
+        for(int i=0;i<searchWord.length();i++)
+        {
+            int index = searchWord[i]-'a';
+            curr = curr->child[index];
+            if(curr!=NULL)
+            {
+                result.push_back(curr->suggestions);
+            }
+            else
+            {
+                result.push_back({});
+            }
+        }
+
         return result;
     }
 };
