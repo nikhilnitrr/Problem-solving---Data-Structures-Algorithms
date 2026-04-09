67cb73883069106858b95ccf49161bd20adc1ae3
Nikhil Chandrakar <48703875+nikhilnitrr@users.noreply.github.com>
2026-04-09 22:39:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,48 +3,46 @@ class Solution
 {
 public:
 
-    int solve(int pos, int k, vector<int>& prices, int cnt, bool holding,
-              vector<vector<vector<int>>>& dp)
+    int solve(int pos, int k, vector<int>&prices, int cnt, bool holding, vector<vector<vector<int>>>&dp)
     {
         if(cnt > k)
-            return -1e9;
-
+        {
+            return INT_MIN;
+        }
         if(pos == prices.size())
-            return holding ? -1e9 : 0;
-
-        if(dp[pos][cnt][holding] != -1)
+        {
+            if(holding)
+            {
+                return INT_MIN;
+            }
+            return 0;
+        }
+        if(dp[pos][cnt][holding]!=-1)
+        {
             return dp[pos][cnt][holding];
-
-        int max_profit = 0;
-
-        if(holding)
-        {
-            // sell
-            max_profit = max(max_profit,
-                prices[pos] + solve(pos+1, k, prices, cnt+1, false, dp));
-
-            // skip
-            max_profit = max(max_profit,
-                solve(pos+1, k, prices, cnt, true, dp));
         }
-        else
+        ll max_profit = 0;
+        for(int i=pos;i<prices.size();i++)
         {
-            // buy
-            max_profit = max(max_profit,
-                solve(pos+1, k, prices, cnt, true, dp) - prices[pos]);
-
-            // skip
-            max_profit = max(max_profit,
-                solve(pos+1, k, prices, cnt, false, dp));
+            if(holding)
+            {
+                max_profit = max(max_profit, (ll)prices[i]+solve(i+1, k, prices, cnt+1, false, dp));
+                max_profit = max(max_profit, (ll)solve(i+1, k, prices, cnt, true, dp));
+            }
+            else
+            {
+                max_profit = max(max_profit, solve(i+1, k, prices, cnt, true, dp)-(ll)prices[i]);
+                max_profit = max(max_profit, (ll)solve(i+1, k, prices, cnt, false, dp));
+            }
         }
-
         return dp[pos][cnt][holding] = max_profit;
     }
 
     int maxProfit(int k, vector<int>& prices) 
     {
+        bool holding = false;
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k+1, vector<int>(2, -1)));
-        return solve(0, k, prices, 0, false, dp);
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(k+1, vector<int>(2, -1)));
+        return solve(0, k, prices, 0, holding, dp);
     }
 };
